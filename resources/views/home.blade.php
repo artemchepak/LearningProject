2994d5f347eb2808ab8b84ea6ca65327b0ba117d
Artem Chepak <a.chepak91@gmail.com>
2021-08-02 11:20:16 +0300

    add category route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,5 +11,7 @@
 
 <h1>Home Page</h1>
 
+<p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda blanditiis cumque dolor eaque enim esse facere fugit inventore ipsum laboriosam maiores qui quia quis recusandae reprehenderit soluta, voluptatem voluptatibus!</span><span>Aliquam at ea in molestiae molestias pariatur porro recusandae sunt ullam voluptas! Accusamus accusantium adipisci consequatur debitis dicta eligendi expedita inventore, itaque natus, odit, pariatur perferendis totam ullam unde vel?</span></p>
+
 </body>
 </html>
