43f14fe2c59c7cd698cfd9f35327cbc65bb65b7f
Artem Chepak <a.chepak91@gmail.com>
2021-08-12 12:11:52 +0300

    start movie base project

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-    <h1>Home page</h1>
-
-    <a href="{{route('notes.index')}}">See all notes</a>
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::redirect('/', '/notes')->name('home');
 
-//Route::get('/notes', [NoteController::class, 'index'])->name('notes');
-//Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
-//Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
-//Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
-//Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
-//Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
-//Route::delete('/notes/{note}', [NoteController::class, 'delete'])->name('notes.delete');
-
-Route::resource('notes', NoteController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
