a63e6524efbc6d93bd2063b375e3299aedd4fb78
Artem Chepak <a.chepak91@gmail.com>
2021-08-12 15:57:36 +0300

    refactor home page markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,23 @@
 
     <h1>Home page</h1>
 
-    @foreach($movies as $movie)
-        <div class="card" style="width: 25rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">{{$movie->title}}</h5>
-                <p class="card-text">{{$movie->genre}}, {{$movie->release_year}}, {{$movie->country}}</p>
-                <a href="{{route('movies.show',$movie->id)}}" class="btn btn-primary">Read more</a>
-            </div>
+
+
+    <div class="container">
+        <div class="d-flex p-2 justify-content-between flex-wrap">
+            @foreach($movies as $movie)
+                <div class="d-inline-flex m-2">
+                    <div class="card" style="width: 20rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$movie->title}}</h5>
+                            <p class="card-text">{{$movie->genre}}, {{$movie->release_year}}, {{$movie->country}}</p>
+                            <a href="{{route('movies.show',$movie->id)}}" class="btn btn-primary">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 
 @endsection
