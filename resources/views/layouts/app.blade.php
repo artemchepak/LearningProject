8794ab2baae676c6d5b290f6f402a22719992644
Artem Chepak <a.chepak91@gmail.com>
2021-08-27 11:42:15 +0300

    add admin panel template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title>MovieBase: @yield('title')</title>
+    @yield('css')
 </head>
 <body>
 
@@ -43,6 +43,12 @@
                             </li>
                         @endif
                     @else
+                        @if(Auth::user()->name == 'Admin')
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="{{ route('admin_panel') }}">Admin panel</a>
+                        </li>
+                        @endif
+
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle  text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
@@ -55,6 +61,7 @@
                                     {{ __('Logout') }}
                                 </a>
 
+
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
                                 </form>
@@ -66,11 +73,14 @@
         </div>
     </div>
 </header>
+<main>
+@yield('side-bar')
 
 <div class="container pt-2">
     @yield('content')
 </div>
-
+</main>
 </body>
 <script src="{{ asset('js/app.js') }}" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class, 'index'])->name('home');
 Route::redirect('/home', '/');
-Route::resource('movies', MovieController::class);
-Route::resource('categories', MovieCategoryController::class);
 Route::get('/movies/category/{id}', [MovieCategoryController::class, 'getAllMoviesByCategory'])->name('movie.by.category');
 
 
@@ -29,6 +27,8 @@ Route::get('test', function (){
 });
 
 Route::middleware(['role:admin'])->prefix('admin_panel')->group(function () {
-    Route::resource('/', \App\Http\Controllers\Admin\HomeController::class);
+    Route::get('/', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin_panel');
+    Route::resource('movies', MovieController::class);
+    Route::resource('categories', MovieCategoryController::class);
 });
 
